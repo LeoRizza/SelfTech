3128543be0a666e487f020004d405e7c84f2bce2
byMeier <mateo.leotta@gmail.com>
2024-01-19 09:58:03 -0300

    Responsive Design 1.0 + NavBar JS

--- js/mateo.js
+++ js/mateo.js
@@ -1,3 +1,20 @@
+let nav = document.querySelector("#nav");
+let abrir = document.querySelector("#abrir")
+let cerrar = document.querySelector("#cerrar")
+
+abrir.addEventListener("click", ()=>{
+    nav.classList.add("visible");
+    abrir.classList.add("ocultar");
+    cerrar.classList.add("visible");
+})
+
+cerrar.addEventListener("click", ()=>{
+    nav.classList.remove("visible");
+    abrir.classList.remove("ocultar");
+    cerrar.classList.remove("visible");
+})
+
+
 $(window).scroll(function(){
     if ($(this).scrollTop() > 100) {
         $('.header').addClass("blur");
